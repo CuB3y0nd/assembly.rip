0a39b993446f43b0a77a5307cfc97d7069c947c9
CuB3y0nd <91041570+CuB3y0nd@users.noreply.github.com>
2024-10-15 23:44:20 +0800

    doc: added site information

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -2,6 +2,78 @@ import type { UserConfig } from '~/types'
 
 export const userConfig: Partial<UserConfig> = {
   // Override the default config here
-  // site: { title: "講評世界" },
-  // seo: { twitter: "@moeyua13" },
+  site: {
+    title: '活版印字',
+    subtitle: 'Typography',
+    author: 'CuB3y0nd',
+    description: '一片没有知识的荒原',
+    website: 'https://assembly.rip/',
+    pageSize: 5,
+    socialLinks: [
+      {
+        name: 'github',
+        href: 'https://github.com/CuB3y0nd',
+      },
+      {
+        name: 'rss',
+        href: '/atom.xml',
+      },
+      // {
+      //   name: 'twitter',
+      //   href: 'https://x.com/CuB3y0nd',
+      // },
+      // {
+      //   name: 'mastodon',
+      //   href: 'https://github.com/moeyua/astro-theme-typography',
+      // },
+    ],
+    navLinks: [
+      {
+        name: 'Posts',
+        href: '/',
+      },
+      {
+        name: 'Archive',
+        href: '/archive',
+      },
+      {
+        name: 'Categories',
+        href: '/categories',
+      },
+      {
+        name: 'About',
+        href: '/about',
+      },
+    ],
+    categoryMap: [{ name: '胡适', path: 'hu-shi' }],
+    footer: [
+      '© %year <a target="_blank" href="%website">%author</a>',
+      'Licensed under <a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">4.0 CC-BY-NC-SA</a>'
+    ],
+  },
+  appearance: {
+    theme: 'light',
+    locale: 'zh-cn',
+    colorsLight: {
+      primary: '#2e405b',
+      background: '#ffffff',
+    },
+    colorsDark: {
+      primary: '#FFFFFF',
+      background: '#232222',
+    },
+    fonts: {
+      header:
+        '"HiraMinProN-W6","Source Han Serif CN","Source Han Serif SC","Source Han Serif TC",serif',
+      ui: '"Source Sans Pro","Roboto","Helvetica","Helvetica Neue","Source Han Sans SC","Source Han Sans TC","PingFang SC","PingFang HK","PingFang TC",sans-serif',
+    },
+  },
+  seo: {
+    twitter: '@CuB3y0nd',
+    meta: [],
+    link: [],
+  },
+  rss: {
+    fullText: true,
+  },
 }
