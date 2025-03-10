871e994eeee2b9e0a972ce28125eaa1ad051c19a
CuB3y0nd <91041570+CuB3y0nd@users.noreply.github.com>
2025-03-11 02:34:55 +0800

    fix: site url configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,7 +25,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://assembly.rip/",
   base: "/",
   trailingSlash: "always",
   integrations: [
